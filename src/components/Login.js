5fbd179b793623896b5f907360df738464617bf3
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-09-26 19:06:27 +0200

    loginpagina werkt nog niet?

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,18 @@
 import React, {useState} from 'react';
-// import Navbar from './components/Navbar.js';
+// import '../App.css';
 
 export default function Login() {
-    // const [isLoggedIn, toggleIsLoggedIn ] = useState(true);
-    // const isLoggedIn = Navbar.isAuthenticated;
-
+    const [isAuthenticated, toggleIsAuthenticated ] = useState(false);
     return (
         <>
             <h1>Login Pagina</h1>
             <p>
                 Button werkt nog niet goed.. is ook anders dan de navbar..Daarom comments zodat de rest wel compiled
             </p>
-            {/*{Navbar.isAuthenticated*/}
-            {/*    ? <button onClick={() => toggleIsAuthenticated(true)}>Inloggen</button>*/}
-            {/*    : <button onClick={() => toggleIsAuthenticated(false)}>Uitloggen</button>*/}
-            {/*}*/}
+            {isAuthenticated
+                ? <button onClick={() => toggleIsAuthenticated(true)}>Inloggen</button>
+                : <button onClick={() => toggleIsAuthenticated(false)}>Uitloggen</button>
+            }
         </>
     );
 }
